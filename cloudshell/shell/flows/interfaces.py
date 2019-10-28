52cd7e6e6738162cb1daf76d35718f9b70e04e72
Kyrylo Maksymenko <saklar13@gmail.com>
2019-10-28 12:33:12 +0200

    remove connectivity

--- cloudshell/shell/flows/interfaces.py
+++ cloudshell/shell/flows/interfaces.py
@@ -3,12 +3,6 @@ from abc import ABCMeta, abstractmethod
 ABC = ABCMeta("ABC", (object,), {"__slots__": ()})
 
 
-class ConnectivityFlowInterface(ABC):
-    @abstractmethod
-    def apply_connectivity_changes(self, request):
-        pass
-
-
 class AutoloadFlowInterface(ABC):
     @abstractmethod
     def discover(self, supported_os, resource_model):
